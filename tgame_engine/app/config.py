ad5f775af7c5d072e827e84ecd6353b48dce6958
vcslav-v <vcslav.v@gmail.com>
2020-10-20 18:30:07 +0300

    add db

--- tgame_engine/app/config.py
+++ tgame_engine/app/config.py
@@ -2,4 +2,9 @@ from os import environ
 
 BOT_TOKEN = environ.get('BOT_TOKEN')
 APP_URL = environ.get('APP_URL')
-SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = (
+    'postgresql+psycopg2://postgres:{password}@{db}/postgres'.format(
+        db=environ.get('DB'),
+        password=environ.get('DB_PASSWORD')
+    )
+)
