fd9991c0ea599056a18edfd05d0189ce6f2117c3
vcslav-v <vcslav.v@gmail.com>
2020-10-21 10:52:18 +0300

    add func get_user

---
+++ tgame_engine/app/db_tools.py
@@ -0,0 +1,22 @@
+"""Database tools."""
+
+from app import models, session
+
+
+def get_user(telegram_id: int) -> models.User:
+    """Return user if one exist or add new user to db.
+
+    Parameters:
+        telegram_id: telegram id
+
+    Returns:
+        User object
+    """
+
+    user = session.query(models.User).filter_by(
+        telegram_id=telegram_id
+    ).first()
+    if user:
+        return user
+    user = models.User(telegram_id=telegram_id)
+    return user
