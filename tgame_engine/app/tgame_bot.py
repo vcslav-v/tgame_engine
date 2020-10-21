0c34dc56b53c38610ca1f02b61eb35ba6c040f8a
vcslav-v <vcslav.v@gmail.com>
2020-10-21 18:29:27 +0300

    fix

--- tgame_engine/app/tgame_bot.py
+++ tgame_engine/app/tgame_bot.py
@@ -1,9 +1,8 @@
 """Bot web points."""
-from app import bot, db_tools
+from app import bot, db_tools, engine
 
 
 @bot.message_handler(commands=['start'])
 def hi_msg(msg):
     user = db_tools.get_user(msg.from_user.id)
-    send_story_message(user)
-    
+    engine.send_story_message(user)
