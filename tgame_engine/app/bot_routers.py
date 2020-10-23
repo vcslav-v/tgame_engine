b96282162a8a3490d0873aa4086a9ad89b0dd88e
vcslav-v <vcslav.v@gmail.com>
2020-10-23 16:54:51 +0300

    add text react

--- tgame_engine/app/bot_routers.py
+++ tgame_engine/app/bot_routers.py
@@ -5,4 +5,10 @@ from app import bot, db_tools, bot_engine
 @bot.message_handler(commands=['start'])
 def hi_msg(msg):
     user = db_tools.get_user(msg.from_user.id)
-    bot_engine.send_story_message(user)
+    bot_engine.tell_story(user)
+
+
+@bot.message_handler(content_types=['text'])
+def text(msg):
+    user = db_tools.get_user(msg.from_user.id)
+    bot_engine.tell_story(user, msg.text)
